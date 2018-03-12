5739d5e74e7607aa6db75a47e11a5fc1d5b93969
MichaelB387 <30610205+MichaelB387@users.noreply.github.com>
2018-03-12 14:49:46 +1100

    comments

--- script.js
+++ script.js
@@ -32,7 +32,8 @@ function start() {
 // Event listeners for keyboard input and the reset button:
 // Event listener "keypress" listens out for when key is pressed
 testArea.addEventListener("keypress", start, false)
-// Event listenr "keyup" listens out for when you let go of the key and spellCheck function will run
+// Event listener "keyup" listens out for when you let go of the key and spellCheck function will run
 testArea.addEventListener("keyup", spellCheck, false)
+// Event listener "click" listens out for when reset button is clicked to run reset function
 resetButton.addEventListener("click", reset, false)
 
